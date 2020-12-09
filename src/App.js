ebdaedd08285ddf635be14d8a6eac970a0645961
Timothy <timothyjhey@gmail.com>
2020-12-10 00:49:09 +0800

    Fix the bug on TileLayer URL

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class App extends Component {
+
+  state = {
+    config: {
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution: '&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }
+  }
+
   render () {
     const icon = new Icon({
       iconUrl: 'https://cdn1.iconfinder.com/data/icons/locations-6/48/1-512.png',
@@ -13,7 +21,7 @@ class App extends Component {
     return (
       <div className="App">
         <MapContainer center={[38.9072, -77.0369]} zoom={1.5}>
-          <TileLayer url="https:{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+          <TileLayer url={this.state.config.url}  attribution={this.state.config.attribution}/>
           <Marker position={[38.9072, -77.0369]} icon={icon}>
             <Popup>
               Hello
