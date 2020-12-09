f6f50f6ffa336f4694002e6a63a2742e88500942
Timothy <timothyjhey@gmail.com>
2020-12-09 21:50:48 +0800

    Deployed to github page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Icon } from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render () {
+    const icon = new Icon({
+      iconUrl: 'https://cdn1.iconfinder.com/data/icons/locations-6/48/1-512.png',
+      iconSize: [30, 30]
+    });
+    return (
+      <div className="App">
+        <MapContainer center={[38.9072, -77.0369]} zoom={1.5}>
+          <TileLayer url="https:{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+          <Marker position={[38.9072, -77.0369]} icon={icon}>
+            <Popup>
+              Hello
+            </Popup>
+          </Marker>
+          
+        </MapContainer>
+      </div>
+    );
+  }
 }
 
 export default App;
